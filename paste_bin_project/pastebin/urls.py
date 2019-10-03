d168aef0b21090fcc4347550d5f9fe3d83f0755b
arpitgupta1906 <gupta.25.iitj.ac.in>
2019-10-03 16:19:27 +0530

    added list view

--- paste_bin_project/pastebin/urls.py
+++ paste_bin_project/pastebin/urls.py
@@ -3,5 +3,6 @@ from .views import PasteCreate,PasteList,PasteDetail
 
 urlpatterns=[
     path('',PasteCreate.as_view(),name="create"),
+    path('pastes/',PasteList.as_view(),name='pastebin_paste_list'),
     path('paste/<int:pk>',PasteDetail.as_view(),name='pastebin_paste_detail'),
 ]
