de80501ddf32157a83e221ee8e4023e4273c5bc2
arpitgupta1906 <gupta.25.iitj.ac.in>
2019-12-14 10:23:26 +0530

    added delete and update urls

--- paste_bin_project/pastebin/urls.py
+++ paste_bin_project/pastebin/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PasteCreate,PasteList,PasteDetail
+from .views import PasteCreate,PasteList,PasteDetail,PasteDelete,PasteUpdate
 
 urlpatterns=[
     path('',PasteCreate.as_view(),name="create"),
     path('pastes/',PasteList.as_view(),name='pastebin_paste_list'),
     path('paste/<int:pk>',PasteDetail.as_view(),name='pastebin_paste_detail'),
+    path('pasteupdate/<int:pk>',PasteUpdate.as_view(),name='pastebin_paste_edit'),
+    path('pastedelete/<int:pk>',PasteDelete.as_view(),name='pastebin_paste_delete'),
 ]
